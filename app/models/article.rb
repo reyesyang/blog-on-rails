a6a988692f2ce01209ebb8bf8849dd8ac8e03318
Reyes Yang <reyes.yang@gmail.com>
2011-06-29 08:33:50 +0800

    Add tags feature for article
    Add tags for article but didn't finish

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,5 +2,9 @@ class Article < ActiveRecord::Base
 	validates :title, :summary, :content, :presence=> true
 	validates :title,	:uniqueness => true
 
-	has_many :comments
+	has_many :comments, :dependent => :destroy
+	has_many :tags
+	
+	accepts_nested_attributes_for :tags, :allow_destroy => :true,
+		:reject_if => proc { |attrs| attrs.all? { |k, v| v.blank?} }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110627141856) do
+ActiveRecord::Schema.define(:version => 20110629000654) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(:version => 20110627141856) do
   create_table "comments", :force => true do |t|
     t.string   "commenter"
     t.text     "content"
+    t.integer  "article_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tags", :force => true do |t|
+    t.string   "name"
     t.integer  "article_id"
     t.datetime "created_at"
     t.datetime "updated_at"

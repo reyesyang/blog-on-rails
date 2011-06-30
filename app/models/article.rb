0999b653005423d3e4c26925c693faecd4e1e84b
Reyes Yang <reyes.yang@gmail.com>
2011-06-30 08:03:23 +0800

    Complete the tags function
    Fix the tags functional error, tags can be add to and remove from article now!

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,7 +3,7 @@ class Article < ActiveRecord::Base
 	validates :title,	:uniqueness => true
 
 	has_many :comments, :dependent => :destroy
-	has_many :tags
+	has_many :tags, :dependent => :destroy
 	
 	accepts_nested_attributes_for :tags, :allow_destroy => :true,
 		:reject_if => proc { |attrs| attrs.all? { |k, v| v.blank?} }

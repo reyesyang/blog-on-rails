77d65026545a745926e0f562fe0db831abfe3e62
Reyes Yang <reyes.yang@gmail.com>
2011-12-27 11:56:57 +0800

    Update the cross security token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Blog::Application.config.secret_token = 'e37108d20fc6ec3bf5806da9cac42555edecde4957c85a0aed085578fdf8c125c9cc719de57e3379b6c18320df8d87efe5a49cac1d42584e3fab5da309793718'
+Blog::Application.config.secret_token = APP_CONFIG['secret_token']
